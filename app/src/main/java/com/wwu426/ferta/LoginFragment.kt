8696819fcbfab55c6a94c26c629ee64ce0ef64ea
Micah Caceres <cacerem@wwu.edu>
2021-06-06 21:38:10 -0700

    added some login and signup input checks

--- app/src/main/java/com/wwu426/ferta/LoginFragment.kt
+++ app/src/main/java/com/wwu426/ferta/LoginFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginFragment : Fragment() {
 
@@ -18,7 +20,20 @@ class LoginFragment : Fragment() {
         var layout = inflater.inflate(R.layout.fragment_login, container, false)
 
         layout.findViewById<Button>(R.id.login_button).setOnClickListener {
-            startActivity(Intent(context, MainActivity::class.java))
+
+            if(layout.findViewById<TextView>(R.id.login_email_et).text.isBlank()){
+                Toast.makeText(requireContext(), "Please enter your email", Toast.LENGTH_SHORT).show()
+            }
+            else if(layout.findViewById<TextView>(R.id.login_password).text.isBlank()){
+                Toast.makeText(requireContext(), "Please enter your password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else {
+                layout.findViewById<TextView>(R.id.login_email_et).text = ""
+                layout.findViewById<TextView>(R.id.login_password).text = ""
+                startActivity(Intent(context, MainActivity::class.java))
+            }
         }
 
         layout.findViewById<Button>(R.id.signup_button).setOnClickListener {
--- app/src/main/java/com/wwu426/ferta/SignUpFragment.kt
+++ app/src/main/java/com/wwu426/ferta/SignUpFragment.kt
@@ -1,5 +1,6 @@
 package com.wwu426.ferta
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,30 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    @SuppressLint("CutPasteId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,7 +23,22 @@ class SignUpFragment : Fragment() {
         var layout = inflater.inflate(R.layout.fragment_sign_up, container, false)
 
         layout.findViewById<Button>(R.id.finish_signup_button).setOnClickListener {
-            startActivity(Intent(context, SetupActivity::class.java))
+
+
+            val password1 = layout.findViewById<TextView>(R.id.signin_password1).text.toString()
+            val password2 = layout.findViewById<TextView>(R.id.signin_password2).text.toString()
+            if(layout.findViewById<TextView>(R.id.signin_email_et).text.isBlank()){
+                Toast.makeText(requireContext(), "Please enter an email", LENGTH_SHORT).show()
+            }
+            else if(password1.isBlank() || password2.isBlank() || password1 != password2){
+                Toast.makeText(requireContext(), "Please enter matching passwords", LENGTH_SHORT).show()
+            }
+            else {
+                layout.findViewById<TextView>(R.id.signin_email_et).text = ""
+                layout.findViewById<TextView>(R.id.signin_password1).text = ""
+                layout.findViewById<TextView>(R.id.signin_password2).text = ""
+                startActivity(Intent(context, SetupActivity::class.java))
+            }
         }
 
         layout.findViewById<Button>(R.id.signup_back_button).setOnClickListener {
@@ -53,23 +52,4 @@ class SignUpFragment : Fragment() {
         return layout
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
