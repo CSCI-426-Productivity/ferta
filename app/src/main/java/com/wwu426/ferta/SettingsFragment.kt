d943c6c6905997227387d83f495cff5bc609eeab
Dan Shtu <shtunyd@wwu.edu>
2021-06-06 21:57:45 -0700

    Logout button actually logs out the user now.

--- app/src/main/java/com/wwu426/ferta/SettingsFragment.kt
+++ app/src/main/java/com/wwu426/ferta/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.wwu426.ferta
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,13 +15,16 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var layout = inflater.inflate(R.layout.fragment_settings, container, false)
-
+        val layout = inflater.inflate(R.layout.fragment_settings, container, false)
+        val buttonLogOut = layout.findViewById<Button>(R.id.button_logout)
         val unavailabilityButton: Button = layout.findViewById(R.id.button_unavailability_change)
+
         unavailabilityButton.setOnClickListener {
             parentFragmentManager.beginTransaction().replace(R.id.settings_view, UnavailableFragment(), "tag").commit()
         }
-
+        buttonLogOut.setOnClickListener {
+            startActivity(Intent(context, AuthActivity::class.java))
+        }
 
         return layout
     }
